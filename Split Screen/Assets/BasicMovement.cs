b598c858a3768fc9778343165d7383789a181c8d
oliviaatthechoir <124530511+oliviaatthechoir@users.noreply.github.com>
2024-02-04 12:24:51 +0100

    split screen done
    weekend was a blur, this should've been done earlier

---
+++ Split Screen/Assets/BasicMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * speed * Time.deltaTime;
+
+        transform.Translate(movement); 
+    }
+}
